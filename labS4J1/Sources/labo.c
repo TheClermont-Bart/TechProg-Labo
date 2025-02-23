e715ba006a6d66632373c0825373122da1d7af9a
Olivier <olivierclermontf@gmail.com>
2025-02-23 07:15:14 -0500

    S4J1 Done!

--- labS4J1/Sources/labo.c
+++ labS4J1/Sources/labo.c
@@ -6,17 +6,17 @@
 /*
 * Creer la fonction d'initialization. Utiliser la fonction allocate au lieu de malloc.
 */
-Stack* new_stack(size_t max_size){
+Stack* new_stack(size_t max_size) {
 	Stack* newStack = (Stack*)allocate(sizeof(Stack));
 	newStack->max_size = max_size;
-	newStack->data = (Stack*)allocate(sizeof(Stack)*newStack->max_size);
+	newStack->data = (Stack*)allocate(sizeof(Stack) * newStack->max_size);
 	newStack->top = -1;
 }
 
 /*
 * Ajouter l'element sur la stack. Prevener "l'overflow" de la stack.
 */
-void push(Stack* s, void* newData){
+void push(Stack* s, void* newData) {
 	if (s->top + 1 < s->max_size) {
 		s->top += 1;
 		s->data[s->top] = newData;
@@ -29,14 +29,14 @@ void push(Stack* s, void* newData){
 /*
 * Enlever l'element de la stack et retourner l'information. Si jamais il n'y a pas d'element, retourner NULL.
 */
-void* pop(Stack* s){
+void* pop(Stack* s) {
 	void* out = NULL;
-	
-	if( s->data == NULL || s->top == -1) {
+
+	if (s->data == NULL || s->top == -1) {
 		return NULL;
 	}
 
-	if ( s->top - 1 >= -1) {
+	if (s->top - 1 >= -1) {
 		out = s->data[s->top];
 		s->top -= 1;
 	}
@@ -46,7 +46,7 @@ void* pop(Stack* s){
 /*
 * Retourner l'element du top sans l'enlever de la stack. Si jamais il n'y a pas d'element, retourner NULL.
 */
-void* peek(Stack* s){
+void* peek(Stack* s) {
 	void* out = pop(s);
 
 	if (out != NULL) {
@@ -62,9 +62,9 @@ void* peek(Stack* s){
 /*
 * Inverser l'ordre de la stack i.e. le haut va devenir le bas.Vous ne devez pas utilise la variable temp qui est une autre Stack.
 */
-void reverseStack(Stack* s){
+void reverseStack(Stack* s) {
 	Stack* newStack = new_stack(s->max_size);
-	
+
 	if (s->data == NULL || s->top == -1) {
 		return NULL;
 	}
@@ -79,17 +79,26 @@ void reverseStack(Stack* s){
 /*
 * Triee la stack.La personne sur le haut/top est la plus jeune. Vous ne pouvez pas modifier la propriete data directement. Vous devez utiliser push,pop et/ou peek.
 */
-void sortStack(Stack* s){
+void sortStack(Stack* s) {
 	Stack* newStack = new_stack(s->max_size);
+	Stack* tempStack = new_stack(s->max_size);
+
+	// DESSINER
 	while (s->top != -1) {
 		Person* temp = pop(s);
 
-		while(newStack->top != -1 && ((Person*)newStack->data[newStack->top])->age > temp->age) {
-			push(s, pop(newStack));
+		while (newStack->top != -1 && temp->age < ((Person*)newStack->data[newStack->top])->age) {
+			push(tempStack, pop(newStack));
 		}
+
 		push(newStack, temp);
+
+		while (tempStack->top != -1) {
+			push(newStack, pop(tempStack));
+		}
 	}
-	while(newStack->top != -1) {
+
+	while (newStack->top != -1) {
 		push(s, pop(newStack));
 	}
 }
