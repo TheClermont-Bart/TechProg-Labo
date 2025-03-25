71ab7795120884663207e5c2540bb122b830252a
Olivier <olivierclermontf@gmail.com>
2025-03-25 17:32:26 -0400

    MAJ Mardi 25 Mars

--- labS8/Sources/labo.c
+++ labS8/Sources/labo.c
@@ -1,1 +1,96 @@
 #include "labo.h"
+
+/*
+* Programmer l'algorithme de fibonacci. Commencer par faire l'algorithme de base et par la suite ajouter la memoization.
+* Faire attention lorsque vous utilisez un type pour une variable qui represente un nombre, int ne depasse pas 2^31-1 ou 2,147,483,647
+*/
+long long memoization[75] = { 0 };
+long long fibonacci(int n) {
+	if (n == 75) {
+		return memoization[n];
+	}
+	if (n <= 1) {
+		return n;
+	}
+	if (memoization[n] != 0 ) {
+		return memoization[n];
+	}
+
+	memoization[n] = fibonacci(n - 1) + fibonacci(n - 2);
+
+	return memoization[n];
+};
+
+/*
+* Creer un Noeud, assigner lui la valeur de data, metter c'est reference a NULL et retourner le noeud.
+*/
+TreeNode* tree_create_node(void* data){
+	TreeNode* Treenode = (TreeNode*)allocate(sizeof(TreeNode));
+
+	Treenode->data = data;
+	Treenode->left = NULL;
+	Treenode->right = NULL;
+
+	return Treenode;
+}
+
+/*
+* Ajouter le noeud  node dans la propriete left ou right qui est NULL de root. On priorise de left a right. Si right et left son NULL on le met dans left.
+* Si jamais aucun des noeuds est NULL, ajouter rien.
+*/
+void tree_add_node(TreeNode* root, TreeNode* node){
+	if (root->left == NULL && root->right == NULL) {
+		root->left = node;
+		return;
+	}
+	if (root->left != NULL && root->right != NULL) {
+		return;
+	}
+	if (root->left == NULL) {
+		root->left = node;
+		return;
+	}
+	if (root->right == NULL) {
+		root->right = node;
+		return;
+	}
+}
+
+/*
+* Programmer l'algorithme de depth first search afin de trouver la cle. Pour programmer cet algorithme, vous avez besoins d'une pile/stack, ca definition est fourni.
+*/
+int dfs(TreeNode* root, void* key){
+	Stack* s;
+	stack_init(s);
+	int nodes_visited = 0;
+
+	if (root->data == key) {
+		nodes_visited++;
+		return nodes_visited;
+	}
+
+	stack_push(s,root);
+
+	while (root->left != NULL && root->data != key) {
+		stack_pop(s);
+		stack_push(s, root->right);
+		stack_push(s, root->left);
+		nodes_visited++;
+		root = root->left;
+	}
+
+	while (root->left == NULL && root->right != NULL && root->data != key) {
+		stack_pop(s);
+		stack_push(s, root->right);
+		stack_push(s, root->left);
+		nodes_visited++;
+		root = root->left;
+	}
+
+	return nodes_visited;
+}
+
+/*
+* Programmer l'algorithme de breath first search afin de trouver la cle. Pour programmer cet algorithme, vous avez besoins d'une file/queue, ca definition est fourni.
+*/
+int bfs(TreeNode* root, void* key){}
--- labS8/Sources/labo.h
+++ labS8/Sources/labo.h
@@ -18,6 +18,12 @@ void* stack_pop(Stack* s);
 * Vous devez definir une noeud que vous appelerez TreeNode. TreeNode va avoir une propriete data lui permettant de recevoir de l'information de different type. 
 * TreeNode a aussi deux propriete de type TreeNode nommer left et right.
 */
+typedef struct TreeNode TreeNode;
+typedef struct TreeNode{
+	void* data;
+	TreeNode* left;
+	TreeNode* right;
+}TreeNode;
 
 
 
